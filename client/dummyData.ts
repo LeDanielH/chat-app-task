b78b8d7fd5eff0b9f6107c96f477f3c564d25177
danielzeman <daniel.zeman@nangu.tv>
2020-07-07 16:51:07 +0200

    dummyData for app design

---
+++ client/dummyData.ts
@@ -0,0 +1,71 @@
+import { TUserMessageDto } from './api/types'
+
+export const DUMMY_PARTICIPANTS: Array<TUserMessageDto['name']> = [
+	'Daniel Zeman',
+	'Nicklas Bendtner',
+	'Silvester Stallone',
+	'Arnold Schwarzenegger'
+]
+
+export const DUMMY_POSTS: Array<TUserMessageDto> = [
+	{
+		name: 'Daniel Zeman',
+		message: `Hello world, I'm here to greet you`,
+		timePostedTimestamp: 1594050739715
+	},
+	{
+		name: 'Allo Lollo',
+		message: `ters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lo`,
+		timePostedTimestamp: 1594050740715
+	},
+	{
+		name: 'Tony KokoLolo',
+		message: `typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+		timePostedTimestamp: 1594050741715
+	},
+	{
+		name: 'Lero Nero',
+		message: `Hello world, I'm here to greet you`,
+		timePostedTimestamp: 1594050742715
+	},
+	{
+		name: 'Daniel Fofr',
+		message: `Hello world, I'm here to greet you`,
+		timePostedTimestamp: 1594050743715
+	},
+	{
+		name: 'Allo Lollo',
+		message: `ters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lo`,
+		timePostedTimestamp: 1594050744715
+	},
+	{
+		name: 'Tony KokoLolo',
+		message: `typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+		timePostedTimestamp: 1594050745715
+	},
+	{
+		name: 'Lero Nero',
+		message: `Hello world, I'm here to greet you`,
+		timePostedTimestamp: 1594050746715
+	},
+	{
+		name: 'Daniel Zeman',
+		message: `Hello world, I'm here to greet you`,
+		timePostedTimestamp: 1594050747715
+	},
+	{
+		name: 'Allo Lollo',
+		message: `ters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lo`,
+		timePostedTimestamp: 1594050748715
+	},
+	{
+		name: 'Tony KokoLolo',
+		message: `typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
+		timePostedTimestamp: 1594050749715
+	},
+	{
+		name: 'Lero Nero',
+		message: `Hello world, I'm here to greet you`,
+		timePostedTimestamp: 1594050750715
+	}
+]
