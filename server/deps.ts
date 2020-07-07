14e417b052ef6828c192e033aef216546c495ef1
danielzeman <daniel.zeman@nangu.tv>
2020-07-07 16:58:53 +0200

    initial dependencies for server side

---
+++ server/deps.ts
@@ -0,0 +1,10 @@
+export { serve } from 'https://deno.land/std/http/server.ts'
+export {
+	acceptWebSocket,
+	isWebSocketCloseEvent,
+	isWebSocketPingEvent,
+	acceptable,
+} from 'https://deno.land/std/ws/mod.ts'
+
+export type { WebSocket, WebSocketMessage } from 'https://deno.land/std/ws/mod.ts';
+
