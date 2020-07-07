fe38d4b2b8288bc871637e8aaab35f0f275a8bbe
danielzeman <daniel.zeman@nangu.tv>
2020-07-08 01:41:52 +0200

    temorarily disabled redux persist -> testing

--- client/app.tsx
+++ client/app.tsx
@@ -1,21 +1,21 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { store } from './config/store'
-import { ReduxPersist } from './store/reduxPersist'
-import _throttle from 'lodash/throttle'
+// import { ReduxPersist } from './store/reduxPersist'
+// import _throttle from 'lodash/throttle'
 import { Provider } from 'react-redux'
 import { Page } from './components/Page'
 import { APP_ROOT_ID } from './constants'
 
-store.subscribe(
-	_throttle(() => {
-		const { messages = [], users = [] } = store.getState();
-		ReduxPersist.saveState({
-			messages,
-			users,
-		})
-	}, 1000)
-)
+// store.subscribe(
+// 	_throttle(() => {
+// 		const { messages = [], users = [] } = store.getState();
+// 		ReduxPersist.saveState({
+// 			messages,
+// 			users,
+// 		})
+// 	}, 1000)
+// )
 
 const Root = () => (
 	<Provider store={store}>
