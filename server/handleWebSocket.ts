1ed2782b7e028c6020a2922e2dc28e61a08485d0
danielzeman <daniel.zeman@nangu.tv>
2020-07-07 20:19:01 +0200

    added ws register logic

--- server/handleWebSocket.ts
+++ server/handleWebSocket.ts
@@ -1,14 +1,24 @@
 import { isWebSocketCloseEvent, WebSocket } from './deps.ts'
 import { broadCastEvents } from './broadCastEvents.ts'
+import { TConnection, TWSData } from "./types.ts";
 
-export const handleWebSocket = (connections: Array<WebSocket>) => async(ws: WebSocket) => {
+export const handleWebSocket = (connections: Array<TConnection>) => async(ws: WebSocket) => {
 	console.log('websocket connection established')
-	connections.push(ws)
 
 	for await (const event of ws) {
 		const isCloseEvent = isWebSocketCloseEvent(event)
 		if (typeof event === 'string') {
-			broadCastEvents(ws, event, connections)
+			const data: TWSData = JSON.parse(event)
+
+			if(data.type === 'register') {
+				connections.push({
+					value: data.value,
+					ws,
+				})
+				ws.send(`${data.value} is registered`)
+			} else {
+				broadCastEvents(ws, event, connections)
+			}
 		}
 
 		if (isCloseEvent) {
