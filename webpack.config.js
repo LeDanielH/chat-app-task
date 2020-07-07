89b5887ccd03286715f8366eda2d026511360080
danielzeman <daniel.zeman@nangu.tv>
2020-07-07 16:26:56 +0200

    initial project config - client

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+
+const prod = process.argv.indexOf('-p') !== -1;
+const path = require('path');
+
+const webpackConfig = {
+    entry: path.join(__dirname, '/client/app.tsx'),
+    output: {
+        path: path.join(__dirname, '/server'),
+        filename: '[name].js',
+        library: 'app',
+        libraryTarget: 'window',
+		publicPath: '/',
+    },
+
+    devtool: prod ? "source-map" : "eval-source-map",
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+                exclude: /(node_modules|Generated|dist|server)/
+            },
+        ]
+    },
+    devServer: {
+        open: true,
+        overlay: true,
+        port: 9876,
+        clientLogLevel: "warning",
+        historyApiFallback: true,
+        inline: true,
+        hot: true,
+        contentBase: './client',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './client/index.html',
+			...prod ? { inlineSource: '.(js|css)$' } : {}
+        }),
+		...prod ? new HtmlWebpackInlineSourcePlugin() : []
+    ]
+};
+
+module.exports = webpackConfig;
