a0cbabd560a88d79e89fab5ce93f5b8532adce90
danielzeman <daniel.zeman@nangu.tv>
2020-07-08 12:36:20 +0200

    more efficient update at utility

--- client/utils/arrayAt.ts
+++ client/utils/arrayAt.ts
@@ -6,8 +6,10 @@ export function removeAt<T>(arr: T[], index: number): T[] {
 	return [...arr.slice(0, index), ...arr.slice(index + 1)]
 }
 
-export function updateAt<T>(arr: T[], updatedObject: T, index: number): T[] {
+export function updateAt<T>(arr: T[], objectPropertiesToUpdate: Partial<T>, index: number): T[] {
+	const objectToUpdate = arr[index];
+
 	const arrayStart = arr.slice(0, index)
 	const arrayEnd = arr.slice(index + 1)
-	return [...arrayStart, updatedObject, ...arrayEnd]
+	return [...arrayStart, {...objectToUpdate, ...objectPropertiesToUpdate }, ...arrayEnd]
 }
