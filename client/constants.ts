a85ba4d9806658236ebfa250f7f437439efefeb8
danielzeman <daniel.zeman@nangu.tv>
2020-07-09 01:17:47 +0200

    enabled different ports for testing on client & on server

--- client/constants.ts
+++ client/constants.ts
@@ -1,9 +1,8 @@
-const isProd = process.argv.indexOf('-p') !== -1
-
 export const APP_ROOT_ID = 'pexip-chat-app'
 export const YOU = 'You'
 export const MEETING_BOT = 'Meeting Bot'
 export const UPDATED_AT = 'Updated at'
 export const MESSAGE_REMOVED = 'Message removed by user'
 export const WS_PORT = 1234
-export const CLIENT_PORT = isProd ? WS_PORT : 9876
+export const CLIENT_PORT = process.env.PORT
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,59 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackInlineSourcePlugin = require('html-webpack-inline-source-plugin')
+const webpack = require('webpack')
 
-const prod = process.argv.indexOf('-p') !== -1;
-const path = require('path');
+const prod = process.argv.indexOf('-p') !== -1
+const path = require('path')
+const PORT = prod ? 1234 : 9876
 
 const webpackConfig = {
-    entry: path.join(__dirname, '/client/app.tsx'),
-    output: {
-        path: path.join(__dirname, '/server'),
-        filename: '[name].js',
-        library: 'app',
-        libraryTarget: 'window',
-		publicPath: '/',
-    },
+	entry: path.join(__dirname, '/client/app.tsx'),
+	output: {
+		path: path.join(__dirname, '/server'),
+		filename: '[name].js',
+		library: 'app',
+		libraryTarget: 'window',
+		publicPath: '/'
+	},
 
-    devtool: prod ? "source-map" : "eval-source-map",
+	devtool: prod ? 'source-map' : 'eval-source-map',
 
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.json']
+	},
 
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'awesome-typescript-loader',
-                exclude: /(node_modules|Generated|dist|server)/
-            },
-        ]
-    },
-    devServer: {
-        open: true,
-        overlay: true,
-        port: 9876,
-        clientLogLevel: "warning",
-        historyApiFallback: true,
-        inline: true,
-        hot: true,
-        contentBase: './client',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './client/index.html',
-			inlineSource: '.(js|css)$',
-        }),
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'awesome-typescript-loader',
+				exclude: /(node_modules|Generated|dist|server)/
+			}
+		]
+	},
+	devServer: {
+		open: true,
+		overlay: true,
+		port: 9876,
+		clientLogLevel: 'warning',
+		historyApiFallback: true,
+		inline: true,
+		hot: true,
+		contentBase: './client'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './client/index.html',
+			inlineSource: '.(js|css)$'
+		}),
 		new HtmlWebpackInlineSourcePlugin(),
-    ]
-};
+		new webpack.DefinePlugin({
+			'process.env.PORT': JSON.stringify(PORT)
+		})
+	],
+	node: {
+		fs: 'empty'
+	}
+}
 
-module.exports = webpackConfig;
+module.exports = webpackConfig
