8d2ac97b87f3985749a24acf20e97281b45cc594
danielzeman <daniel.zeman@nangu.tv>
2020-07-10 03:12:57 +0200

    added error messages to constants

--- client/constants.ts
+++ client/constants.ts
@@ -5,3 +5,9 @@ export const UPDATED_AT = 'Updated at'
 export const MESSAGE_REMOVED = 'Message removed by user'
 export const WS_PORT = 1234
 export const CLIENT_PORT = process.env.PORT
+
+export const SERVER_NOT_AVAILABLE = 'Server not available'
+export const UNABLE_TO_SEMD = `${SERVER_NOT_AVAILABLE}. Unable to send the message.`
+export const UNABLE_TO_REMOVE = `${SERVER_NOT_AVAILABLE}. Unable to remove the message.`
+export const UNABLE_TO_REGISTER = `${SERVER_NOT_AVAILABLE}. Cannot register.`
+export const CANNOT_BE_EMPTY = 'Input cannot be empty'
